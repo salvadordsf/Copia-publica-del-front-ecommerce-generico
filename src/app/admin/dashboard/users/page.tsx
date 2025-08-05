edea1bc442bc5df927f209e96d28b22092f5bb6e
salvadordsf <salvadordisabatto@gmail.com>
2025-08-05 09:37:27 -0300

    feat: add base structure for /users route and placeholder for user search

---
+++ src/app/admin/dashboard/users/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ProductsPage() {
+  return (
+    <main className="flex flex-col gap-7 mt-4 pr-5">
+      <h2 className="text-2xl font-bold">Buscar Usuario</h2>
+      <p>Agregar buscador de usuarios aqui</p>
+    </main>
+  );
+}
