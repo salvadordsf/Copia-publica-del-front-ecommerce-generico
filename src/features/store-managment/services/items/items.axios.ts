cd8628ff41fa02e55652b368f44cf7af32af1ca0
salvadordsf <salvadordisabatto@gmail.com>
2026-01-06 11:20:39 -0300

    feat(store-manager): create the axios methods for /home/items-sections

---
+++ src/features/store-managment/services/items/items.axios.ts
@@ -0,0 +1,46 @@
+import axiosInstance from "@/lib/axios/axios";
+import {
+  ICreateItem,
+  IGetItem,
+  IUpdateItem,
+} from "../../schemas/items/items-schema";
+
+export const getItems = async (data: IGetItem) => {
+  const params: any = {
+    ...(data?.sectionId && { sectionId: data?.sectionId }),
+    ...(data?.itemType && { itemType: data?.itemType }),
+    ...(data?.productId && { productId: data?.productId }),
+    ...(data?.categoryId && { categoryId: data?.categoryId }),
+  };
+
+  const res = await axiosInstance.get("/home/items-sections", {
+    params,
+  });
+  console.log(res);
+  return res.data;
+};
+
+export const getItemById = async (id: string) => {
+  const res = await axiosInstance.get(`/home/items-sections/${id}`);
+  console.log(res);
+  return res.data;
+};
+
+export const createItem = async (data: ICreateItem) => {
+  const res = await axiosInstance.post("/home/items-sections", data);
+  console.log(res);
+  return res.data;
+};
+
+export const updateItem = async (id: string, data: IUpdateItem) => {
+  const res = await axiosInstance.put(`/home/items-sections/${id}`, data);
+  console.log(data);
+  console.log(res);
+  return res.data;
+};
+
+export const deleteItem = async (id: string) => {
+  const res = await axiosInstance.delete(`/home/items-sections/${id}`);
+  console.log(res);
+  return res.data;
+};
