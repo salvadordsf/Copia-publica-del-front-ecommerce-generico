da60aa97044c80f73219e0a69287201c9cfa0234
salvadordsf <salvadordisabatto@gmail.com>
2026-01-08 12:28:54 -0300

    feat(home-store): create the page for the /sections/ID for the section dedicated page by id in the url, including the SectionInfo component

---
+++ src/app/admin/dashboard/home-store/sections/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { SectionInfo } from "@/features/store-managment/components/sections/section/section-info";
+
+export default function SectionEditorPage() {
+  return (
+    <main className="flex flex-col gap-7 mt-4 pr-5">
+      <section>
+        <SectionInfo />
+      </section>
+    </main>
+  );
+}
