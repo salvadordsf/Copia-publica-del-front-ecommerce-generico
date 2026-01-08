87d1d90177b6a6b02675da4f2a60f2842a7af649
salvadordsf <salvadordisabatto@gmail.com>
2026-01-08 12:05:50 -0300

    feat(home-store): add the storeViewer comp for the /home-store for view the steate of the home sections

--- src/app/admin/dashboard/home-store/page.tsx
+++ src/app/admin/dashboard/home-store/page.tsx
@@ -1,21 +1,12 @@
-import UiDivider from "@/components/dashboard/divider/divider";
-import OrderSearcher from "@/features/orders/components/searcher/orders-searcher";
+import { StoreViewer } from "@/features/store-managment/components/overviews/store-viewer";
 
-
-export default function ProductsPage() {
+export default function HomeStorePage() {
   return (
     <main className="flex flex-col gap-7 mt-4 pr-5">
       <section>
-        <h2 className="text-2xl font-bold">Buscar órdenes</h2>
-        <OrderSearcher />
+        <h2 className="text-2xl font-bold">Estado de la tienda</h2>
+        <StoreViewer />
       </section>
-
-      {/* <UiDivider />
-
-      <section>
-        <h2 className="text-2xl font-bold">Crear orden</h2>
-        
-      </section> */}
     </main>
   );
 }
