15cf9322fc52c7a160ad5f8129217bc8caef743e
salvadordsf <salvadordisabatto@gmail.com>
2026-01-08 12:30:05 -0300

    feat(home-store): create the layout and page for the /sections path where the user can manage the sections using the StoreEditor component

---
+++ src/app/admin/dashboard/home-store/sections/page.tsx
@@ -0,0 +1,12 @@
+import { StoreEditor } from "@/features/store-managment/components/overviews/store-editor";
+
+export default function SectionEditorPage() {
+  return (
+    <main className="flex flex-col gap-7 mt-4 pr-5">
+      <section>
+        <h2 className="text-2xl font-bold">Estado de la tienda</h2>
+        <StoreEditor />
+      </section>
+    </main>
+  );
+}
