896d1622b06a49323270ba10cacc048d5e203c73
salvadordsf <salvadordisabatto@gmail.com>
2026-01-21 14:03:04 -0300

    refact: minor changes in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,9 @@
-import { MainHeroCarousel } from "@/components/public-store/home/hero/main-hero";
-import { CategoriesSlider } from "@/components/public-store/home/sliders/categories/categories-slider";
-import { RelevantProductsSlider } from "@/components/public-store/home/sliders/relevant-products/relevant-products-slider";
+import { HomePage } from "@/features/store-managment/components/home/home-page";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <MainHeroCarousel />
-      </main>
-      <section className="m-auto max-w-3xl">
-        <CategoriesSlider />
-        <RelevantProductsSlider />
-      </section>
+      <HomePage />
     </>
   );
 }
