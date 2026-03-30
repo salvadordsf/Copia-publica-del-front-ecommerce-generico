60fcb5af17b9c2519ccc65d2133cb28adcb4dee5
salvadordsf <salvadordisabatto@gmail.com>
2026-03-30 10:35:10 -0300

    feat(add-product-item-form): improve typing in props
    - AddProductItemForm: correctly typed `sectionId`, `items`, and `closeDialog`
    - Ensures autocomplete and type safety when passing props to ProductPreview

--- src/features/admin/store-managment/components/items/item-forms/product/add-product.tsx
+++ src/features/admin/store-managment/components/items/item-forms/product/add-product.tsx
@@ -1,24 +1,12 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import {
-  CreateImageItemSchema,
-  ICreateImageItem,
-} from "@/features/admin/store-managment/schemas/items/items-schema";
-import { useCreateItem } from "@/features/admin/store-managment/services/items/items-mutations";
-import { toast } from "sonner";
-import { toastError } from "@/utils/toast-error-utility";
-import { AxiosError } from "axios";
 import ProductBulkFilters from "@/features/admin/products/components/filter/products-filters";
 import ProductPreview from "./product-preview";
-import MethodsBtns from "@/components/dashboard/btns/btn-request-method";
+import { ItemSection } from "@/types/resources/home-section-types";
 
 interface Props {
   sectionId: string;
-  items: any[];
+  items: ItemSection[];
   closeDialog: () => void;
 }
 
