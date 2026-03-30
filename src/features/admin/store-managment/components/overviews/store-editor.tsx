4b4ed355345a3fab0c9c74fa633ecfe5f3ba143a
salvadordsf <salvadordisabatto@gmail.com>
2026-03-30 10:28:55 -0300

    refactor(typing): improve TypeScript types in StoreEditor
    - Typed sections data using HomeSection
    - Ensures type safety when mapping and rendering SectionCard components

--- src/features/admin/store-managment/components/overviews/store-editor.tsx
+++ src/features/admin/store-managment/components/overviews/store-editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { HomeSection } from "@/types/resources/home-section-types";
 import { useSections } from "../../services/sections/sections-query";
 import { AddSection } from "../sections/cards/add-section";
 import { SectionCard } from "../sections/cards/section-card";
@@ -8,7 +9,8 @@ import { StoreViewerContainer } from "./store-viewer-conteiner";
 interface IStoreEditorProps {}
 
 export const StoreEditor = ({}: IStoreEditorProps) => {
-  const { data: data = {}, isLoading, error } = useSections({});
+  const { data, isLoading, error } = useSections({});
+  const sections = data?.success ? data.data : [];
 
   if (isLoading) return <p>Cargando secciones...</p>;
   if (error) return <p>Error al cargar secciones</p>;
@@ -16,9 +18,9 @@ export const StoreEditor = ({}: IStoreEditorProps) => {
   return (
     <StoreViewerContainer>
       <>
-        {data.data.length > 0 ? (
-          data.data.map((section: any) => (
-            <SectionCard section={section} key={section.key} />
+        {sections.length > 0 ? (
+          sections.map((section: HomeSection) => (
+            <SectionCard section={section} key={section.id} />
           ))
         ) : (
           <div className="text-center">Aún no hay secciones creadas</div>
