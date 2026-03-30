ae51b63f843edbfed4bb135a688403c142e3bada
salvadordsf <salvadordisabatto@gmail.com>
2026-03-30 10:30:28 -0300

    refactor(typing): improve TypeScript types in UpdateTextItemForm
    - Typed props, form methods, and mutation data using IUpdateItemText
    - Ensures type safety for section, item, and form fields

--- src/features/admin/store-managment/components/items/item-forms/update-text.tsx
+++ src/features/admin/store-managment/components/items/item-forms/update-text.tsx
@@ -23,24 +23,21 @@ interface Props {
 
 export function UpdateTextItemForm({ item, closeDialog }: Props) {
   const {
-    data: { data: section } = {},
+    data,
     isLoading: isLoadingSection,
     error: errorSection,
   } = useSectionById(item.sectionId);
+  const section = data?.success ? data.data : null;
 
   const { mutate, isPending } = useUpdateItems<IUpdateItemText>(item.id);
-
-  const updatedItem = section.items.find(
-    (updated: ItemSection) => item.id === updated.id,
-  );
 
   const methods = useForm<IUpdateItemText>({
     resolver: zodResolver(UpdateItemTextSchema),
     defaultValues: {
-      title: updatedItem.title ?? undefined,
-      subtitle: updatedItem.subtitle ?? undefined,
-      linkUrl: updatedItem.linkUrl ?? undefined,
-      position: updatedItem.position,
+      title: item.title ?? undefined,
+      subtitle: item.subtitle ?? undefined,
+      linkUrl: item.linkUrl ?? undefined,
+      position: item.position,
     },
   });
 
