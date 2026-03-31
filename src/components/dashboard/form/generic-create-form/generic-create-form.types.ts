7c128b7ae83a49039890d27b8cd4eae0c8172976
salvadordsf <salvadordisabatto@gmail.com>
2026-03-30 23:25:08 -0300

    feat(type): Enhance types for GenericForm and apply them to inputs
    - Refined `GenericFormField` and `IGenericFormProps` with precise types for each input variant.
    - Added support for specific defaultValue types (e.g., `OrderStatus`, `TUserRoles`, `string[]`, `number`).
    - Updated `GenericForm` component to fully leverage improved typing with React Hook Form `Controller` and `useFormContext`.
    - Ensured all inputs (Select, Slider, Textarea, RadioGroup, ToggleCreateTagInput, Input) correctly accept typed `field` props.

--- src/components/dashboard/form/generic-create-form/generic-create-form.types.ts
+++ src/components/dashboard/form/generic-create-form/generic-create-form.types.ts
@@ -1,9 +1,20 @@
+import { OrderStatus } from "@/types/resources/order-types";
+import { TUserRoles } from "@/types/roles/users-roles.types";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
 export interface GenericFormField {
   name: string;
   label: string;
   selectLabel?: string;
   placeholder?: string;
-  type: "text" | "number" | "select" | "slider" | "textarea" | "radio group" | "toggle tag";
+  type:
+    | "text"
+    | "number"
+    | "select"
+    | "slider"
+    | "textarea"
+    | "radio group"
+    | "toggle tag";
   options?: {
     value: string | boolean;
     label: string;
@@ -13,8 +24,18 @@ export interface GenericFormField {
   ux?: {
     uxMinMax?: boolean;
     uxSteps?: boolean;
-  }
-  defaultValue?: any;
+  };
+  defaultValue?: 
+    // for ToggleCreateTagInput input in GenericForm
+    (string[] | undefined) | 
+    // for UiSlider input in GenericForm
+    number | 
+    // for OrderStatus input in UpdateOrderDialog + Relevance input in CreateProductForm
+    OrderStatus | 
+    // for User Role input in UpdateUserDialog
+    TUserRoles | 
+    // for CategoryId and SubcategoryId inputs in UpdateProductDialog + User name in UpdateUserDialog 
+    string;
   min?: number;
   max?: number;
   step?: number;
@@ -23,13 +44,13 @@ export interface GenericFormField {
   className?: string;
 };
 
-export interface IGenericFormProps {
+export interface IGenericFormProps<TFormValues> {
   fields: GenericFormField[];
   submitButtonText: string;
   submitButtonType: "search" | "create" | "update" | "delete";
   btnClassName?: string;
-  onSubmitAction: (data: any) => void;
+  onSubmitAction: SubmitHandler<FieldValues> | ((data: TFormValues) => void) | (() => void);
   isError?: boolean;
-  serverError?: { status: number } | any;
+  serverError?: { status: number } | unknown;
   className?: string;
 }
