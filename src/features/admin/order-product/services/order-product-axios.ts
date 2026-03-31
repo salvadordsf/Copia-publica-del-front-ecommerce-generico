d71bf4564388ce484572051ae1c4f29e3329fd8a
salvadordsf <salvadordisabatto@gmail.com>
2026-03-30 22:59:32 -0300

    feat(public-store/orders): improve typing for order-products API
    - Add explicit return type for `createOrderProduct`
    - Ensure `createManyOrderProducts` returns typed array of ApiResponse
    - Remove ambiguous `any` usage in bulk creation function

--- src/features/admin/order-product/services/order-product-axios.ts
+++ src/features/admin/order-product/services/order-product-axios.ts
@@ -1,21 +1,24 @@
 import axiosInstance from "@/lib/axios/axios";
 import { ICreateOrderProduct } from "../schemas/order-product-schema";
+import { ApiResponse } from "@/types/responses.type";
+import { IOrder, IOrderProduct } from "@/types/resources/order-types";
 
-export const createOrderProduct = async (data: ICreateOrderProduct) => {
+export const createOrderProduct = async (
+  data: ICreateOrderProduct,
+): Promise<ApiResponse<{ orderProduct: IOrderProduct; order: IOrder }>> => {
   const res = await axiosInstance.post("/order-products", data);
   console.log(res);
   return res.data;
 };
 
-export const createManyOrderProducts = async (
-  items: ICreateOrderProduct[]
-) => {
+export const createManyOrderProducts = async (items: ICreateOrderProduct[]) => {
   const results = [];
 
   for (const item of items) {
     const created = await createOrderProduct(item);
     results.push(created);
+    console.log("as");
   }
-
+  console.log(results);
   return results;
 };
