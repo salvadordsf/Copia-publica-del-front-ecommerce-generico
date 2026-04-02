b73d8d8ba6082c8aa85e876eef0834a3072e50ad
salvadordsf <salvadordisabatto@gmail.com>
2026-04-02 01:32:04 -0300

    fix(auth): fix the auth cookies auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  fetchOptions: {
+    credentials: "include" as RequestCredentials,
+  },
 });
